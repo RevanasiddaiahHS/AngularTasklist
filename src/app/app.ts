95039183c06053976db277411bb87089e639f99e
Revana Siddaiah <Revana Siddaiah@LAPTOP-VT657LCV>
2025-08-05 22:45:32 +0530

    basic project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  
   protected readonly title = signal('first-angular-app');
 }
+
