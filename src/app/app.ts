7bb17e33135fb00b611a5e40a2d36f8553a6c2a3
Revana Siddaiah <Revana Siddaiah@LAPTOP-VT657LCV>
2025-08-07 11:08:43 +0530

    Using the emitted value and passing to different component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,22 @@ import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
   protected readonly title = signal('first-angular-app');
+  get selectedUser() {
+    return this.users.find((u) => u.id === this.selectedUserId);
+  }
   onSelectUser(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
